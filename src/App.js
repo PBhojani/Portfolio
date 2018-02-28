1052119702e84118e35457687e4ed193975590de
Separated the project for easier loaded solution <Palak.Bhojani@ardentmc.com>
2018-02-28 12:53:29 -0500

    Push

--- src/App.js
+++ src/App.js
@@ -6,20 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-		<nav class="navbar navbar-inverse bg-inverse navbar-collapse collapse">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <h4>Palak Bhojani</h4>
-          </div>
-            <ul class="nav navbar-nav" role="nav">
-              <li><NavLink to="/Portfolio/" onlyActiveOnIndex={true}>About</NavLink></li>
-              <li><NavLink to="/Portfolio/static/js/modules/experience">Experience</NavLink></li>
-              <li><NavLink to="/Portfolio/static/js/modules/education">Education</NavLink></li>
-              <li><NavLink to="/Portfolio/static/js/modules/skills">Skills</NavLink></li>
-              <li><NavLink to="/Portfolio/static/js/modules/portfolios">Portfolios</NavLink></li>
-            </ul>
-        </div>
-		</nav>
+		  Hello Router!
 		</div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-import Education from './modules/Education';
-import Portfolios from './modules/Portfolios';
-import Experience from './modules/Experience';
 import About from './modules/About';
-import Skills from './modules/Skills';
 
 ReactDOM.render((
 <Router history={browserHistory}>
     <Route path={process.env.PUBLIC_URL + '/'} component={App}>
       <IndexRoute component={About} />
-      <Route path="/Portfolio/static/js/modules/portfolios" component={Portfolios}/>
-      <Route path="/Portfolio/static/js/modules/experience" component={Experience}/>
-      <Route path="/Portfolio/static/js/modules/education" component={Education}/>
-      <Route path="/Portfolio/static/js/modules/skills" component={Skills}/>
-    </Route>
+      </Route>
   </Router>
 ), document.getElementById('root'));
 registerServiceWorker();
